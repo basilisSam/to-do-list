a4e051f0239045a2ba32525729578374c61caa58
Sam <Sam@DESKTOP-2IHO4FC>
2021-09-30 17:52:18 +0300

    create input field

---
+++ src/components/InputField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InputFields() {
+  return (
+    <div className='pt-8'>
+      <input type='text' className='border-2 border-red-500 ' />
+    </div>
+  );
+}
+
+export default InputFields;
