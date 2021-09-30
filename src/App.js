66c7359b9c08166f3f67801f6a3665f330a67c8e
Sam <Sam@DESKTOP-2IHO4FC>
2021-09-30 18:11:50 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import InputField from "./components/InputField";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className='App'>
       <h1 className='font-sans text-5xl'>THINGS TO DO</h1>
       <InputField />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className='bg-black
+        text-3xl text-white text-center
+        border-t-4 border-gray-500
+        fixed
+        inset-x-0
+        bottom-0
+        p-4
+        h-40'
+    >
+      Sam Footer
+    </div>
+  );
+};
+
+export default Footer;
