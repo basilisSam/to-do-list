754434039798e0bc2297b4b9409af0a8dcb93d49
Sam <Sam@DESKTOP-2IHO4FC>
2021-10-01 19:15:20 +0300

    fetching data for todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import InputField from "./components/InputField";
 import Footer from "./components/Footer";
+import TodoList from "./components/TodoList";
 function App() {
   return (
     <div className='App'>
       <h1 className='font-sans text-5xl'>THINGS TO DO</h1>
       <InputField />
       <Footer />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function TodoList() {
-  return <div></div>;
-}
+const TodoList = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/todos")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
+  console.log(data);
+  return (
+    <div>
+      {data.map((todos) => (
+        <p key={todos.id}>{todos.title}</p>
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
