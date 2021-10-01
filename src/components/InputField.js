041f11e2fcd13076e27c98224687f39e7faec180
Sam <Sam@DESKTOP-2IHO4FC>
2021-10-01 16:07:48 +0300

    add save button

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function InputFields() {
   return (
-    <div className='pt-8'>
-      <input type='text' className='border-2 border-red-500 ' />
+    <div className='pt-8 '>
+      <form className='flex place-content-center'>
+        <input type='text' className='border-2 border-red-500' />
+        <input
+          type='submit'
+          value='Save'
+          className='bg-red-600 text-white w-20 text-center'
+        />
+      </form>
     </div>
   );
 }
