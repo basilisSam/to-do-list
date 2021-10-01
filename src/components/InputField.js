a4fdb28fbfc9154f9af33dea8107372d7bc044d5
Sam <Sam@DESKTOP-2IHO4FC>
2021-10-01 18:10:55 +0300

    Add style to InputFields

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,12 +3,12 @@ import React from "react";
 function InputFields() {
   return (
     <div className='pt-8 '>
-      <form className='flex place-content-center'>
-        <input type='text' className='border-2 border-red-500' />
+      <form className='flex place-content-center space-x-4'>
+        <input type='text' className='border-2 border-red-500 rounded-lg  ' />
         <input
           type='submit'
           value='Save'
-          className='bg-red-600 text-white w-20 text-center'
+          className='bg-red-600 text-white w-20 text-center rounded-3xl space-x-8'
         />
       </form>
     </div>
